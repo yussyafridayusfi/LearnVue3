f9c1be9f81cb3eb9fc2892f96e2ec151e17a6d84
yussyafridayusfi <yussyafrida0299@gmail.com>
2025-10-09 15:26:48 +0700

    init master learn vite vue3

---
+++ web-vite/src/components/FooterSection.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="footer">
+    <p>© 2025 MyLanding. All rights reserved.</p>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  padding: 20px;
+  text-align: center;
+  background: #1e3a8a;
+  color: white;
+}
+</style>
